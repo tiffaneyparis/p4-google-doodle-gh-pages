bd4510f44dbced5642e9b69b7cfd74c55990363d
Tiffaney Paris <tiffaneyparis@Tiffaneys-MacBook-Pro.local>
2016-06-06 17:16:03 -0700

    initial on laptop

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// animate star //
+
+var $star = $('#star');
+
+//selector or DOM element, duration, CSS properties
+
+TweenMax.from($star, 1, {
+    
+    opacity: 0,
+    x: 180
+    
+    
+    
+});
